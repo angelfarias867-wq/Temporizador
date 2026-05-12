3062ae0fc83c43f0475a2c97d9a76b129d538d79
angelfarias867-wq <angelfarias867@gmail.com>
2026-05-12 13:16:54 -0400

    fuap1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const tiempoInput = document.getElementById('tiempoInput');
-const botonIniciar = document.getElementById('botonIniciar');
-const relojDisplay = document.getElementById('reloj');
+const tiempo = document.getElementById('tiempoInput');
+const btnIniciar = document.getElementById('botonIniciar');
+const reloj = document.getElementById('reloj');
 const cohete = document.getElementById('cohete');
 
 let cuentaRegresiva;
 
-botonIniciar.addEventListener('click', () => {
-    let tiempoRestante = parseInt(tiempoInput.value);
+btnIniciar.addEventListener('click', () => {
+    let tiempoRestante = parseInt(tiempo.value);
 
     if (isNaN(tiempoRestante) || tiempoRestante <= 0 || tiempoRestante > 60) {
         alert("Por favor, ingresa un número entre 1 y 60.");
@@ -15,11 +15,11 @@ botonIniciar.addEventListener('click', () => {
    
     clearInterval(cuentaRegresiva); // resetear estados previos (por si ya despegó)
     cohete.classList.remove('despegar');
-    relojDisplay.innerText = tiempoRestante;
+    reloj.innerText = tiempoRestante;
 
     cuentaRegresiva = setInterval(() => {// Iniciar el temporizador
         tiempoRestante--;
-        relojDisplay.innerText = tiempoRestante;
+        reloj.innerText = tiempoRestante;
 
         if (tiempoRestante <= 0) { // Cuando llegue a cero
             clearInterval(cuentaRegresiva);
@@ -29,8 +29,8 @@ botonIniciar.addEventListener('click', () => {
 });
 
 function lanzamiento() {
-    relojDisplay.innerText = "¡IGNICIÓN!";
-    relojDisplay.style.color = "#2ecc71";
+    reloj.innerText = "¡IGNICIÓN!";
+    reloj.style.color = "#2ecc71";
     
     cohete.classList.add('despegar');
 }
